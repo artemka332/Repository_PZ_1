2c1b756f04395fd652da22d6fa3577bd835c75c3
artemka332 <artemkasashin@yandex.ru>
2025-09-17 20:08:43 +0300

    add comments

--- module_64_8.sv
+++ module_64_8.sv
@@ -2,24 +2,28 @@
 
 module module_64_8
 (
-    input   clk,
-    input   reset_n,
-    input   strobe_in,
-    input   [63:0] input_data,
-    input   req_data,
-    output  ready,
-    output data_end,
-    output  strobe_out,
-    output  [7:0] data_out
+    input   clk,                // сигнал синхронизации
+    input   reset_n,            // сигнал сброса
+    input   strobe_in,          // сигнал строба
+    input   [63:0] input_data,  // входные данные
+    input   req_data,           // запрос на вывод данных
+    output  ready,              // сигнал готовности
+    output data_end,            // сигнал конца строки
+    output  strobe_out,         // сигнал строба на выход
+    output  [7:0] data_out      // выходные данные
 );
 
 // память для модуля fifo
 reg [63:0] mem [7:0];
 
+// указатели чтения и записи данных в fifo
 reg [3:0] wr_adr;
 reg [3:0] rd_adr;
+
+// сигнал разрешения чтения данных
 wire read_enable;
 
+// сигнал полного и пустого fifo
 wire full = (wr_adr[2:0] == rd_adr[2:0]) && (wr_adr[3]!= rd_adr[3]);
 wire empty = wr_adr == rd_adr;
 
@@ -28,7 +32,7 @@ assign ready = empty;
 // изменение указателя записи данных
 always @(posedge clk)
 begin
-    if(!reset_n)
+    if(!reset_n) // сброс адреса записи
     begin
        wr_adr <= 0; 
     end
@@ -37,7 +41,7 @@ begin
     begin
         if(strobe_in && !full)
         begin
-          wr_adr <= wr_adr + 1; 
+          wr_adr <= wr_adr + 1; // увеличение счётчика записи
         end
     end
 end
@@ -47,7 +51,7 @@ always @(posedge clk)
 begin
     if(strobe_in && !full)
     begin
-        mem[wr_adr[2:0]] <= input_data;
+        mem[wr_adr[2:0]] <= input_data; // запись данных
     end
 end
 
@@ -63,11 +67,12 @@ begin
     begin
         if(read_enable)
         begin
-           rd_adr <= rd_adr + 1; 
+           rd_adr <= rd_adr + 1; // увеличение адреса чтения на 1
         end
     end
 end
 
+// промежуточный буффер для данных из fifo
 reg [63:0] data_out_fifo;
 
 always @(posedge clk)
@@ -81,12 +86,12 @@ begin
     begin
         if(read_enable)
         begin
-           data_out_fifo <=  mem[rd_adr[2:0]]; 
+           data_out_fifo <=  mem[rd_adr[2:0]]; // запись в промежуточный буффер
         end
     end
 end
 
-
+// конечный автомат вывода данных
 FSM_out FSM_out_1
 (
     .clk            (clk),
